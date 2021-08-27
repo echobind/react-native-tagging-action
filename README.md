390bf7d00444106c2f7f51e43fb237cad1f10d5d
Dominic Sherman <dominic.sherman98@gmail.com>
2021-08-27 10:12:45 -0700

    Update documentation

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # React Native Tagging Action
+
+This action automates the GitHub version tagging and releasing process for mobile applications.
+
+Versions tagged by this action will be of the form `v1.0.0-1`. For more details on this, read about the [Tagging Process](#tagging-process).
+## Usage
+
+```yaml
+- uses: echobind/react-native-tagging-action
+  with:
+    # A personal access token for the user that will be tagging and publishing the releases
+    github-auth-token: '****'
+    # The name of the branch the tag should be created off of (defaults to 'main')
+    branch-to-tag: 'main'
+    # Type of version change to make ('major', 'minor', 'patch', 'none' - defaults to 'none')
+    version-change-type: 'none'
+```
+
+For an example workflow using this action, refer to [the example](https://github.com/DominicSherman/react-native-tagging-action/blob/master/.github/workflows/example.yml).
+
+## Tagging Process
+
+The process for tagging utilized by this action is specific to mobile apps and allows for the usual major, minor, and patch version bumps, as well as no version bump.
+
+As an example, if your most recent tagged version is `v1.0.0-1`:
+* A **major** bump will result in `v2.0.0-1`
+* A **minor** bump will result in `v1.1.0-1`
+* A **patch** bump will result in `v1.0.1-1`
+* No bump being indicated will result in `v1.0.0-2`.
+
+It is done this way to allow for multiple tagged releases of a mobile app within the same version, since the actual version of the app (`1.0.0` in this scenario) only needs to be changed once the previous version has been published publicly. For example, there could be 5 different releases on version `1.0.0` that are sent to internal/beta testers only but not published.
+
+This also allows for scheduled tagging/releasing of your app internally with no bump specified by default.
+
+## Releasing
+
+In addition to creating a tag off of the HEAD of the specified branch, a release will also be created in GitHub for the given tag. The title of this release will be of the form `Release ${versionNumber}`, and the body will include a list of all commits to the branch since the previous tag. Commits will be listed in the form `${commitMessage} ${sha} - ${author}`. 
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const fetch = require('node-fetch');
 const core = require('@actions/core');
 
+/**
+ * Bump version based on version change type
+ * @param {string} tag ex: 1.0.0
+ * @returns updated version (ex: 2.0.0)
+ */
 const getNewTag = tag => {
-    const versionChangeType = core.getInput('version-change-type');
+    // get input from github action, default to 'none'
+    const versionChangeType = core.getInput('version-change-type') || 'none';
 
     const [major, minor, patch] = tag.split('.');
 
@@ -18,8 +24,9 @@ const getNewTag = tag => {
 };
 
 const createTag = async () => {
+    // get inputs from github action, with defaults
     const githubAuthToken = core.getInput('github-auth-token');
-    const branchToTag = core.getInput('branch-to-tag');
+    const branchToTag = core.getInput('branch-to-tag') || 'main';
 
     const headers = {
         Accept: 'application/vnd.github.v3+json',
@@ -27,37 +34,49 @@ const createTag = async () => {
     };
 
     try {
-        const response = await fetch(
+        // get existing tags
+        const tagsResponse = await fetch(
             `${process.env.GITHUB_API_URL}/repos/${process.env.GITHUB_REPOSITORY}/tags`,
             {headers}
         );
-        const data = await response.json();
+        const tagsData = await tagsResponse.json();
 
+        // default if the repository doesn't have any tags created yet
         let mostRecentTag = 'v0.0.1-0';
 
-        if (data && Array.isArray(data) && data[0] && data[0].name) {
-            mostRecentTag = data[0].name;
+        if (tagsData && Array.isArray(tagsData) && tagsData[0] && tagsData[0].name) {
+            // specify most recent tag if there are existing tags in the repo
+            mostRecentTag = tagsData[0].name;
         }
 
+        // get commits to HEAD since most recent tag
         const commitSinceTagUrl = `${process.env.GITHUB_API_URL}/repos/${process.env.GITHUB_REPOSITORY}/compare/${mostRecentTag}...HEAD`;
         const commitsSinceTagResponse = await fetch(commitSinceTagUrl, {headers});
         const commitSinceTagData = await commitsSinceTagResponse.json();
 
+        // generate list of commit messages for release body
         const commitMessages = commitSinceTagData.commits.map((item) =>
             `* ${item.commit.message} ${item.html_url} - @${item.author.login}`
         );
 
+        // break version out from version count (v1.0.0-1 -> v1.0.0 and 1)
         const [tagVersion, tagVersionNumber] = mostRecentTag.split('-');
+        // remove 'v' from version
         const cleanTag = tagVersion.replace('v', '');
+        // get updated version based on version change type
         const newTag = getNewTag(cleanTag);
+        // if the version didn't change, then bump the version count
         const updatedTagVersionNumber =
             cleanTag === newTag ? Number(tagVersionNumber) + 1 : 1;
+        // put it all back together
         const tagName = `v${newTag}-${updatedTagVersionNumber}`;
 
         const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
 
-        const body = `${commitMessages.join('\n* ')}`;
+        // generate body for release
+        const body = `${commitMessages.join('\n ')}`;
 
+        // create release
         await fetch(
             `${process.env.GITHUB_API_URL}/repos/${process.env.GITHUB_REPOSITORY}/releases`,
             {
@@ -66,7 +85,7 @@ const createTag = async () => {
                     repo,
                     tag_name: tagName,
                     target_commitish: branchToTag,
-                    name: `${tagName}`,
+                    name: `Release ${tagName}`,
                     body,
                 }),
                 method: 'POST',
@@ -74,7 +93,6 @@ const createTag = async () => {
             }
         );
     } catch (error) {
-        console.log('error', error);
         core.setFailed(error.message);
     }
 };
